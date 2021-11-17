ac099745d522ab626102d7d834640fa48b97f9a5
viksingh <vikas.singh.0@gmail.com>
2021-11-17 19:28:19 +1100

    Create randomize_order.py

---
+++ randomize_order.py
@@ -0,0 +1,14 @@
+
+import random
+array = ["Miran","Sasen","George","Jeev","Eddy","Vihaan","Ishaan","Kris"]
+
+random.shuffle(array)
+
+print("First player",array[0])
+print("Second player",array[1])
+print("Third player",array[2])
+print("Fourth player",array[3])
+print("Fifth player",array[4])
+print("Sixth player",array[5])
+print("Seventh player",array[6])
+print("Eighth player",array[7])
