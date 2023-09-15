b214e9ef4a2724cf78acb708019012cd77491cc0
viksingh <1918060+viksingh@users.noreply.github.com>
2023-09-15 21:39:12 +1000

    Update and rename randomize_order.py to randomize_orders.py

---
+++ randomize_orders.py
@@ -0,0 +1,14 @@
+import random
+array = ["Miran","Joseph","Liam","Jeev","Mihit","Vihaan","Alexander","Flynn","Monish"]
+
+random.shuffle(array)
+
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+print(array[5])
+print(array[6])
+print(array[7])
+print(array[8])
